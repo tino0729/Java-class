f2570f18efd8bcff6cbdde5f99ac43de94e8a14a
tino0729 <cderosa1@ivytech.edu>
2023-08-28 11:25:38 -0500

    Chapter 1 "you do it"

---
+++ YouDoItChpt1/Hello2.java
@@ -0,0 +1,10 @@
+public class Hello2 
+{
+    /* This class demonstrates the use of the println() method to print the message Hello, world! */
+    // Rest of your code will go here
+
+public static void main(String[] args)
+{
+    System.out.println("Hello World");
+}
+}
---
+++ YouDoItChpt1/Hellodialog.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+public class Hellodialog 
+{
+    // Filename Hellodialog.java
+    // Written by Clemete De Rosa
+    // Written on 8/24/23
+    // Page 29
+
+    public static void main(String[] args) 
+    {
+        JOptionPane.showMessageDialog(null,  "Hello, WorldD!");
+
+}
+}
